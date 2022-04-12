d40f4a38efcb6c9187df637da397c8fabd0b2dcf
cyebukayire <peacecyebukayire@gmail.com>
2022-04-12 23:54:55 +0200

    create navbar

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {About, Footer, Header, Skills, Testimonial, Work} from './container'
+import { Navbar } from "./components";
 
 const App = () => {
     return(
         <div className="App">
+            <Navbar />
             <Header />
             <About />
             <Work />
