9167ca8d9e59c3a65222b81415d3e3c88c64009e
cyebukayire <peacecyebukayire@gmail.com>
2022-04-13 02:48:29 +0200

    update navbar

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
 import './Navbar.scss'
+import { images } from '../../constants'
+
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <img src={images.logo} alt="" />
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) =>(
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
