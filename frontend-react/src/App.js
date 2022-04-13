590ecca311dddd7af7fb178bfafbf3dafca2d62f
cyebukayire <peacecyebukayire@gmail.com>
2022-04-13 03:18:04 +0200

    add client

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import {About, Footer, Header, Skills, Testimonial, Work} from './container'
-import { Navbar } from "./components";
-import './App.scss'
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
-const App = () => {
-    return(
-        <div className="app">
-            <Navbar />
-            <Header />
-            <About />
-            <Work />
-            <Skills />
-            <Testimonial />
-            <Footer />
-        </div>
-    )
-};
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
 
 export default App;
--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-// 00:24:05
