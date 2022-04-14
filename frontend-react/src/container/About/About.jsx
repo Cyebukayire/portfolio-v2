6a7eac369661eb3124c58e72a0357e7c265b664f
cyebukayire <peacecyebukayire@gmail.com>
2022-04-14 10:20:37 +0200

    chore: fetch about data from sanity

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import { images } from '../../constants'
 import './About.scss'
 
-const abouts = [
-  { title: 'Fullstack Development', description: "I am a good web developer.", imgUrl: images.about01 },
-  { title: 'Database Management', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'DevOps', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'UI/UX Design', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'AI and ML', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'Game Development', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'Networking Systems', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'Embedded Systems', description: "I am a good web developer.", imgUrl: images.about03 },
-  { title: 'IOT', description: "I am a good web developer.", imgUrl: images.about03 }
+import { urlFor, client } from '../../client'
 
-]
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
     <h2 className='head-text'> I Know that <span>Good Apps</span> <br /> means <span>Good Business</span> </h2>
@@ -29,7 +28,7 @@ const About = () => {
           className="app__profile-item"
           key={about.title + index}
         >
-          <img src={about.imgUrl} alt={about.title} />
+          <img src={urlFor(about.imgUrl)} alt={about.title} />
           <h2 className='field-title bold-text' style={{ marginTop: 20}}>{ about.title }</h2>
           <h2 className='p-text' style={{ marginTop: 10}}>{ about.description }</h2>
         </motion.div>
@@ -41,4 +40,4 @@ const About = () => {
 
 export default About
 
-// 1:31:59
+// 1:44:30
