d712c8835e26abd3d4ba49391d4e1a0dc6fbf243
cyebukayire <peacecyebukayire@gmail.com>
2022-04-18 19:07:47 +0200

    update socialmedia

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
-// import { FaFacebookF } from 'react-icons/fa';
+// import React from 'react';
+// import { BsTwitter, BsGithub, BsLinkedin } from 'react-icons/bs';
+// // import { FaFacebookF } from 'react-icons/fa';
 
-const SocialMedia = () => (
-  <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <BsLinkedin />
-    </div>
-    <div>
-      <BsGithub />
-    </div>
-  </div>
-);
+// const SocialMedia = () => (
+//   <div className="app__social">
+//     <div>
+//       <BsTwitter />
+//     </div>
+//     <div>
+//       <BsLinkedin />
+//     </div>
+//     <div>
+//       <BsGithub />
+//     </div>
+//   </div>
+// );
 
-export default SocialMedia;
+// export default SocialMedia;
