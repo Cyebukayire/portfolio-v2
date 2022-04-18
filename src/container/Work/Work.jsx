beb6bdaf272587bf02f225e8dd3a69a1d83cf841
cyebukayire <peacecyebukayire@gmail.com>
2022-04-18 03:48:17 +0200

    update containers

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -38,10 +38,10 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">My <span>Portfolio</span> </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['Web App', 'Mobile App', 'React JS', 'Python', 'AI & ML', 'UI/UX', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
